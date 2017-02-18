1c590410a0ca08eead43d54f0929146bf5625cb2
Cakedy <Cakedy@users.noreply.github.com>
2017-02-18 08:32:52 -0500

    Created HelloWorld.java
    You need the JDK to compile and run this.

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+	  Note: You need the JDK to compile and run this.
+	Author: Cakedy Cake
+	  Date: Feb-18-2017
+*/
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		char H = 'H';
+		char e = 'e';
+		char l = 'l';
+		char o = 'o';
+		char W = 'W';
+		char r = 'r';
+		char d = 'd';
+		char space = ' ';
+		char exclamationMark = '!';
+		
+		System.out.println(""+H+e+l+l+o+space+W+o+r+l+d+exclamationMark);
+	}
+}
